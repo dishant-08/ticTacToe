4f3162eb3ee64c0d2eb1a39172a5b20e88ee438c
Dishant Sahu <dishant.sahu07@gmail.com>
2023-11-26 02:10:54 +0530

    added bot

--- game.js
+++ game.js
@@ -1,48 +1,41 @@
-const ticTacToe = (xName , oName) => {
-    
-    const X = "X" //:snake
-    const O = "O"
-    let currentPlayer = "X";
-    // console.log(xName);
-  
-    const players = {
-        X : xName,
-        O : oName
-    }
+const ticTacToe = (xName, oName) => {
+  const X = "X"; //:snake
+  const O = "O";
+  let currentPlayer = "X";
+  // console.log(xName);
 
-    // lookUp Table nextPlayer[currplayer] (Object hai yeh)
-    const nextPlayer = {  
-    X : "O",
-    O : "X"
-    }
+  const players = {
+    X: xName,
+    O: oName,
+  };
 
-    const board = [
-        // "Ongoing",
-        // "", "", "",
-        // "", "", "",
-        // "", "", "",
-    ];
+  // lookUp Table nextPlayer[currplayer] (Object hai yeh)
+  const nextPlayer = {
+    X: "O",
+    O: "X",
+  };
 
-    for(let i=1 ;i <=9 ; i++){
-        board[i] = ""
-    }
+  const board = [
+    // "Ongoing",
+    // "", "", "",
+    // "", "", "",
+    // "", "", "",
+  ];
 
+  for (let i = 1; i <= 9; i++) {
+    board[i] = "";
+  }
 
-    
- 
+  function isValidmove(move) {
+    return 1 <= move && move <= 9 && board[move] === "";
+  }
 
-    function isValidmove(move) {
+  function computeStatus() {
+    let result = "ongoing"; // this represent the status of game : ongoing , win-x , win-O , draw
 
-     return (1<=move && move<=9) && board[move] === "";
-    }
+    //   console.log(winningCombos)
 
-     function computeStatus() {
-
-        let result = "ongoing"; // this represent the status of game : ongoing , win-x , win-O , draw
- 
-        //   console.log(winningCombos)
-
-       /* let winningCombos =[
+    /* let winningCombos =[
             [ 1, 2, 3 ],
             [ 4, 5, 6 ],
             [ 7, 8, 9 ],
@@ -53,154 +46,132 @@ const ticTacToe = (xName , oName) => {
             [ 3, 5, 7 ]
           ]; */
 
-          const generateCombos = (n) => {
-  
-            let res = [];
-            
-            let grid = [];
-            let v = 1;
-          
-            
-            if(n == 1) {
-              
-              let arr = [1];
-              res.push(arr)
-               return res;
-            }
-          
-          
-           
-            // Create a 2D grid and populate it with values from 1 to n^2
-            for (let i = 0; i < n; i++) {
-              grid[i] = [];
-              for (let j = 0; j < n; j++) {
-                grid[i][j] = v++;
-              }
-            }
-          
-            // Push each row to the result
-            for (let i = 0; i < n; i++) {
-              res.push(grid[i]);
-            }
-          
-            // Push each column to the result
-            for (let i = 0; i < n; i++) {
-              let cols = [];
-              for (let j = 0; j < n; j++) {
-                cols.push(grid[j][i]);
-              }
-              res.push(cols);
-            }
-          
-            // Push the main diagonal to the result
-            let diagonal = [];
-            for (let i = 0; i < n; i++) {
-              diagonal.push(grid[i][i]);
-              // console.log(diagonal);
-            }
-            
-            res.push(diagonal);
-          
-            // Push the back diagonal to the result
-            let backDiagonal = [];
-            for (let i = 0; i < n; i++) {
-              for (let j = 0; j < n; j++) {
-                if (i + j === n - 1) {
-                  backDiagonal.push(grid[i][j]);
-                }
-              }
-            }
-            res.push(backDiagonal);
-            
-          //   console.log(res)
-          
-            return res;
+    const generateCombos = (n) => {
+      let res = [];
+
+      let grid = [];
+      let v = 1;
+
+      if (n == 1) {
+        let arr = [1];
+        res.push(arr);
+        return res;
+      }
+
+      // Create a 2D grid and populate it with values from 1 to n^2
+      for (let i = 0; i < n; i++) {
+        grid[i] = [];
+        for (let j = 0; j < n; j++) {
+          grid[i][j] = v++;
+        }
+      }
+
+      // Push each row to the result
+      for (let i = 0; i < n; i++) {
+        res.push(grid[i]);
+      }
+
+      // Push each column to the result
+      for (let i = 0; i < n; i++) {
+        let cols = [];
+        for (let j = 0; j < n; j++) {
+          cols.push(grid[j][i]);
+        }
+        res.push(cols);
+      }
+
+      // Push the main diagonal to the result
+      let diagonal = [];
+      for (let i = 0; i < n; i++) {
+        diagonal.push(grid[i][i]);
+        // console.log(diagonal);
+      }
+
+      res.push(diagonal);
+
+      // Push the back diagonal to the result
+      let backDiagonal = [];
+      for (let i = 0; i < n; i++) {
+        for (let j = 0; j < n; j++) {
+          if (i + j === n - 1) {
+            backDiagonal.push(grid[i][j]);
           }
-          // console.log(res)
-          // Checking X or O wins
-          let winningCombos = generateCombos(3)
-          
-          let x , flag;
-          winningCombos.forEach(([i1 , i2 , i3] ) => {
+        }
+      }
+      res.push(backDiagonal);
 
-          if(board[i1]===board[i2] && board[i2]===board[i3] && board[i3]===currentPlayer){
+      //   console.log(res)
 
+      return res;
+    };
+    // console.log(res)
+    // Checking X or O wins
+    let winningCombos = generateCombos(3);
+
+    let x, flag;
+    winningCombos.forEach(([i1, i2, i3]) => {
+      if (
+        board[i1] === board[i2] &&
+        board[i2] === board[i3] &&
+        board[i3] === currentPlayer
+      ) {
         // console.log(`win -${currentPlayer}`);
-            flag =1;
-           x =`win - ${currentPlayer}`;
-        }
-    })
-    if(flag === 1)
-    return x;
+        flag = 1;
+        x = `win - ${currentPlayer}`;
+      }
+    });
+    if (flag === 1) return x;
 
+    // Checking for Draw ( MY OWN METHOD)
+    //   let areAllPositionTaken = false;
+    //   let m = 1;
+    //   for(let i=1 ; i<=9 ; i++){
+    //       if(board[i]!= ""){
+    //            m = m + 1;
+    //            if(m == 10){
+    //          areAllPositionTaken =true
+    //            }
+    //         }
+    //     }
+    //     m=1;
+    // if(areAllPositionTaken){
+    //     return "Draw"
+    // }
 
-   
-          // Checking for Draw ( MY OWN METHOD)
-        //   let areAllPositionTaken = false;
-        //   let m = 1;
-        //   for(let i=1 ; i<=9 ; i++){
-        //       if(board[i]!= ""){
-        //            m = m + 1;
-        //            if(m == 10){
-        //          areAllPositionTaken =true
-        //            }
-        //         }
-        //     }
-        //     m=1;
-        // if(areAllPositionTaken){
-        //     return "Draw"
-        // }
+    // Checking for Draw  (OPTIMIZE SOLUTION)
+    let areAllPositionsTaken = true;
 
-        // Checking for Draw  (OPTIMIZE SOLUTION)
-        let areAllPositionsTaken = true;
-
-        for (let i = 1; i <= 9; i++) {
-            if (board[i] === "") {
-                areAllPositionsTaken = false;
-                break; // Exit the loop as soon as a vacant position is found
-            }
-        }
-
-        if (areAllPositionsTaken) {
-            return "Draw";
-        }
-
-
-
-        
-        
-        return result;
+    for (let i = 1; i <= 9; i++) {
+      if (board[i] === "") {
+        areAllPositionsTaken = false;
+        break; // Exit the loop as soon as a vacant position is found
+      }
     }
 
+    if (areAllPositionsTaken) {
+      return "Draw";
+    }
 
+    return result;
+  }
 
-
-  
-
-    return (player,move) => {
-      
+  return (player, move) => {
     // validate right player n move
-    if (player !== currentPlayer){
-       return [false , `Not your turn ,  ${currentPlayer}'s turns now `]
-    }
-        
-    if (!isValidmove(move)){
-       return[false, `Invalid move ,Try Again`]
+    console.log(`${player} plays at ${move}`);
+    if (player !== currentPlayer) {
+      return [false, `Not your turn ,  ${currentPlayer}'s turns now `];
     }
 
-    board[move]= currentPlayer;
-    board[0]= computeStatus();
-    currentPlayer = nextPlayer[currentPlayer]
+    if (!isValidmove(move)) {
+      return [false, `Invalid move ,Try Again`];
+    }
 
-  
+    board[move] = currentPlayer;
+    board[0] = computeStatus();
+    currentPlayer = nextPlayer[currentPlayer];
 
-    
-    return[true , board];
- }
+    return [true, board, currentPlayer];
+  };
 };
 
-
-
-module.exports= {ticTacToe}
-
-
+module.exports = { ticTacToe };
--- playGame.js
+++ playGame.js
@@ -1,29 +1,39 @@
-
-
-const {ticTacToe} = require("./game");
-
+const { ticTacToe } = require("./game");
 
 let play = ticTacToe("Fedora", "Dishant");
 
-
-
 const printBoard = (board) => {
-    console.log(`------${board[0]}------`)
-    for (let i = 1; i <= 7; i += 3) {
-         console.log(`${board[i] || i} | ${board[i+1] || i+1} | ${board[i+2]|| i+2}`)
-        if (i < 7) {
-            console.log("-----------");
-        }
+  console.log(`------${board[0]}------`);
+  for (let i = 1; i <= 7; i += 3) {
+    console.log(
+      `${board[i] || i} | ${board[i + 1] || i + 1} | ${board[i + 2] || i + 2}`
+    );
+    if (i < 7) {
+      console.log("-----------");
     }
+  }
 };
 
+function ticBot(empty) {
+  let move = Math.floor(Math.random() * empty.length);
+  // console.log(move);
+  return empty[move];
+}
 
+function findEmpty(board) {
+  let arr = [];
+  for (let i = 1; i <= 9; i++) {
+    if (board[i] == "") {
+      arr.push(i);
+    }
+  }
+  return arr;
+}
 
-let boardorMessage, result;
+let boardorMessage, result, currentPlayer;
 
-
-[result, boardorMessage] = play("X",1);
-[result, boardorMessage] = play("O",6);
+[result, boardorMessage, currentPlayer] = play("X", 1);
+// [result, boardorMessage] = play("O", 6);
 // //  printBoard(boardorMessage);
 // [result, boardorMessage] = play("X",5);
 // [result, boardorMessage] = play("O",2);
@@ -38,8 +48,28 @@ let boardorMessage, result;
 // [result, boardorMessage] = play("O",8);
 // printBoard(boardorMessage);
 
-if(result){
-    printBoard(boardorMessage);
-}else{
+// [result, boardorMessage] = play("X", 1);
+// printBoard(boardorMessage);
+while (true) {
+  let empty = [];
+  if (result) {
+    empty = findEmpty(boardorMessage);
+    // console.log(empty);
+    if (!empty.length) {
+      break;
+    }
+    [result, boardorMessage, currentPlayer] = play(
+      currentPlayer,
+      ticBot(empty)
+    );
+  } else {
     console.log(boardorMessage);
+    break;
+  }
 }
+printBoard(boardorMessage);
+// if (result) {
+//   printBoard(boardorMessage);
+// } else {
+//   console.log(boardorMessage);
+// }
